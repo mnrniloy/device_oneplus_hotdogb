bdd2e9d4da1a45871909e54dd4c435fb4563b96c
mnrniloy <mnrniloy0@gmail.com>
2023-12-31 23:59:21 +0600

    hotdogb: Update Dependencies

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+# Clone/Fetch Upstream Device Dependencies
+# Device Tree
+echo "Cloning Vendor blobs"
+git clone https://github.com/yaap/vendor_oneplus_hotdogb.git-b fourteen vendor/oneplus/hotdogb
+echo ""
